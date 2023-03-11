60b1d8af234bcf19fc5bb9d6176e91c5335debb6
David112x <86davidx64@gmail.com>
2023-03-11 17:16:01 -0500

    Initialize QCOM-EDK2

---
+++ QcomPkg/Library/ImageVersionLib/oem_version.c
@@ -0,0 +1,18 @@
+/*
+#============================================================================
+#  Name:                                                                     
+#    oem_version.c 
+#
+#  Description:                                                              
+#    None 
+#                                                                            
+# Copyright (c) 2023 Qualcomm Technologies, Inc. All Rights Reserved        
+#============================================================================
+#                                                                            
+# *** AUTO GENERATED FILE - DO NOT EDIT                                      
+#                                                                            
+# GENERATED: Thu Mar  9 08:52:18 2023 
+#============================================================================
+*/
+const char IMAGE_VARIANT_STRING_AUTO_UPDATED[]="IMAGE_VARIANT_STRING=Sdm660LA";
+const char OEM_IMAGE_VERSION_STRING_AUTO_UPDATED[]="OEM_IMAGE_VERSION_STRING=iMacPro2019";
